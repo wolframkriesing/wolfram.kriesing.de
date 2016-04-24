16f04c9d2d394ff6d505ed4066a0a5632b982972
Wolfram Kriesing <wolfram+github@kriesing.de>
2016-04-24 18:17:46 +0200

    Render the first article.

--- ROADMAP.md
+++ ROADMAP.md
@@ -1,8 +1,8 @@
 # TODOs
 
 - separate the development of the system and the content (at least into separate branches)
-- [DONE] make headlines linkable (hover and get a link that I can copy into tweets or so)
-- [DONE] allow tagging - tags using lists at the end of article
+- make headlines linkable (hover and get a link that I can copy into tweets or so)
+- allow tagging - tags using lists at the end of article
 - show items grouped/paging kinda
 - show "read later" or "watch later" lists
 - allow "* later" to be ticked off
--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
-import fs from 'fs';
-import {parse} from './markdown-converter.js';
-
-const fileContent = fs.readFileSync(__dirname + '/../content/tech.md', 'utf8');
-
-const articles = parse(fileContent).articles;
-//console.log(JSON.stringify(articles, null, 4));
+import ReactDOM from 'react-dom';
+import { parse } from './markdown-converter.js';
+import fetch from 'isomorphic-fetch'
 
 import React from 'react';
 import ArticlePage from './components/ArticlePage.js';
+// import ArticleList from './components/ArticleList.js';
 
-const rendered = React.renderToString(<ArticlePage articles={articles}/>);
-const indexHtml = fs.readFileSync(__dirname + '/index.html', 'utf8');
-console.log(indexHtml + rendered);
+const withFileContent = (fileContent) => {
+  const articles = parse(fileContent).articles;
+  console.log(JSON.stringify(articles[0], null, 4));
+  
+  // ReactDOM.render(<ArticleList articles={ articles }/>, document.getElementById('app'));
+  ReactDOM.render(<ArticlePage articles={ [articles[0]] }/>, document.getElementById('app'));
+};
+
+fetch('./content/tech.md')
+  .then(fileContent => fileContent.text())
+  .then(withFileContent)
+;
+
+
+// const rendered = React.renderToString(<ArticlePage articles={articles}/>);
+// const indexHtml = fs.readFileSync(__dirname + '/index.html', 'utf8');
+// console.log(indexHtml + rendered);
