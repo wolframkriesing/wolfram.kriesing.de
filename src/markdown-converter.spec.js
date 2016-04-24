c2f3452aae7675462330cd7d4f9580f6c9700218
Wolfram Kriesing <wolfram+github@kriesing.de>
2016-04-24 19:05:30 +0200

    Small CS.

--- src/markdown-converter.spec.js
+++ src/markdown-converter.spec.js
@@ -1,5 +1,5 @@
 import assert from 'assert';
-import {parse} from '../markdown-converter.js';
+import { parse } from './markdown-converter.js';
 
 describe('markdown document parsing', function() {
   describe('containing one article', function() {
@@ -15,12 +15,12 @@ paragraph
 * tag1
 * tag2
     `;
-    
+
     let parsed;
     beforeEach(function() {
       parsed = parse(md);
     });
-    
+
     it('finds 1 article', () => {assert.equal(parsed.articles.length, 1);});
 
     describe('the article has', function() {
@@ -31,25 +31,25 @@ paragraph
       it('correct headline', () => {assert.equal(article.headline, 'headline');});
       it('two tags', () => {assert.deepEqual(article.tags, ['tag1', 'tag2']);});
 
-      describe('the content has', function() {  
+      describe('the content has', function() {
         let content;
         beforeEach(function() {
           content = article.content;
         });
         it('3 items', () => {assert.equal(content.length, 3);});
-        
+
         it('first a quote', () => {assert.equal(content[0].type, 'blockquote');});
         it('the right quote text', () => {assert.equal(content[0].text, 'quote');});
-        
+
         it('second a paragraph', () => {assert.equal(content[1].type, 'paragraph');});
         it('pure paragraph', () => {assert.equal(content[1].text, 'paragraph');});
-        
+
         it('third a code snippet', () => {assert.equal(content[2].type, 'code');});
         it('code content', () => {assert.equal(content[2].text, 'code');});
       });
     });
   });
-  
+
   describe('differently structured article', function() {
     const md = `
 # headline
@@ -64,7 +64,7 @@ paragraph 2
 
 * tags
     `;
-    
+
     let parsed;
     beforeEach(function() {
       parsed = parse(md);
@@ -74,9 +74,9 @@ paragraph 2
         headline: 'headline',
         tags: ['tags'],
         content: [
-          {type: 'paragraph', text: 'paragraph 1'},
-          {type: 'blockquote', text: 'quote\nquote\nquote'},
-          {type: 'paragraph', text: 'paragraph 2'}
+          { type: 'paragraph', text: 'paragraph 1' },
+          { type: 'blockquote', text: 'quote\nquote\nquote' },
+          { type: 'paragraph', text: 'paragraph 2' }
         ]
       };
       assert.deepEqual(parsed.articles[0], expected);
@@ -103,11 +103,11 @@ paragraph 2
 # headline 3
 paragraph 3
       `;
-      it('has three articles', 
+      it('has three articles',
         () => {assert.deepEqual(parse(md).articles.length, 3); });
-      it('second has two contents', 
+      it('second has two contents',
         () => {assert.deepEqual(parse(md).articles[1].content.length, 2); });
-      it('second article, content[1]=blockquote', 
+      it('second article, content[1]=blockquote',
         () => {assert.deepEqual(parse(md).articles[1].content[1].type, 'blockquote'); });
     });
   });
